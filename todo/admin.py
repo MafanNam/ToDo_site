219403a629d1a481239b484b603db662bf4b0cd5
Maxim Manzik <kepkpkep@gmail.com>
2023-05-25 04:57:50 +0300

    add task funct

---
+++ todo/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task', 'is_completed', 'updated_at')
+    search_fields = ('task', )
